d4c39f03582f1a786777b444d54d1bedbfaf8f33
cxfcxf <siegfried.chen@gmail.com>
2015-08-12 22:53:55 -0700

    system api and RunCmds update

--- goeapi-commands.go
+++ goeapi-commands.go
@@ -29,14 +29,14 @@ func (ns *Nodes) ParseConfig(file string) {
     }
 }
 
-func (n *Node) RunCmds(cmds []string) string {
+func (n *Node) RunCmds(cmds []string, format string) string {
     data, err := json.Marshal(map[string]interface{}{
             "jsonrpc": "2.0",
             "method": "runCmds",
             "params": map[string]interface{} {
                 "version": 1,
                 "cmds": cmds,
-                "format": "json",
+                "format": format,
                 },
             "id": 1,
         })
@@ -56,11 +56,11 @@ func (n *Node) RunCmds(cmds []string) string {
 }
 
 func (n *Node) Enable(cmds []string) string {
-    return n.RunCmds(append([]string{"enable"}, cmds...))
+    return n.RunCmds(append([]string{"enable"}, cmds...), "json")
 }
 
 func (n *Node) Configure(cmds []string) string {
-    return n.RunCmds(append([]string{"enable", "configure"}, cmds...))
+    return n.RunCmds(append([]string{"enable", "configure"}, cmds...), "json")
 }
 
 func (n *Node) Running_config() string {
--- goeapi-vlan.go
+++ goeapi-vlan.go
@@ -1,53 +1,64 @@
 package main
 
 import (
-	"fmt"
-	"strconv"
+    "fmt"
+    "strconv"
 )
 
 func IsVlan(vid string) bool {
-	id, err := strconv.Atoi(vid)
-	if err != nil {
-		panic(err)
-	}
-	if id >= 1 && id <= 4095 {
-		return true
-	}
-	return false
+    id, err := strconv.Atoi(vid)
+    if err != nil {
+        panic(err)
+    }
+    if id >= 1 && id <= 4095 {
+        return true
+    }
+    return false
 }
 
 func (n *Node) VlanGet(vid string) string {
-	if IsVlan(vid) {
-		command := fmt.Sprint("show vlan %s", vid)
-		return n.Configure([]string{command})
-	}
-	return "vlan id not valid"
+    if IsVlan(vid) {
+        command := fmt.Sprint("show vlan %s", vid)
+        return n.Configure([]string{command})
+    }
+    return "vlan id not valid"
 }
 
 func (n *Node) VlanGetAll() string {
-	return n.Configure([]string{"show vlan"})
+    return n.Configure([]string{"show vlan"})
 }
 
 func (n *Node) VlanCreate(vid string) string {
-	if IsVlan(vid) {
-    	command := fmt.Sprintf("vlan %s", vid)
-    	return n.Configure([]string{command})
-	}
-	return "vlan id not valid"
+    if IsVlan(vid) {
+        command := fmt.Sprintf("vlan %s", vid)
+        return n.Configure([]string{command})
+    }
+    return "vlan id not valid"
 }
 
 func (n *Node) VlanDelete(vid string) string {
-	if IsVlan(vid) {
-    	command := fmt.Sprintf("no vlan %s", vid)
-    	return n.Configure([]string{command})
+    if IsVlan(vid) {
+        command := fmt.Sprintf("no vlan %s", vid)
+        return n.Configure([]string{command})
     }
     return "vlan id not valid"
 }
 
 func (n *Node) VlanConfigure(vid string, cmds []string) string {
-	if IsVlan(vid) {
-		inszero := fmt.Sprintf("vlan %s", vid)
-		return n.Configure(append([]string{inszero}, cmds...))
-	}
-	return "vlan id not valid"
+    if IsVlan(vid) {
+        inszero := fmt.Sprintf("vlan %s", vid)
+        return n.Configure(append([]string{inszero}, cmds...))
+    }
+    return "vlan id not valid"
 }
+
+func (n *Node) AddTrunkGroups(vid string, name string) string {
+    ins := fmt.Sprintf("trunk group %s", name)
+    return n.VlanConfigure(vid, []string{ins})
+}
+
+func (n *Node) RemoveTrunkGroups(vid string, name string) string {
+    ins := fmt.Sprintf("no trunk group %s", name)
+    return n.VlanConfigure(vid, []string{ins})
+}
+
